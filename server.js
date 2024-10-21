d8a28caf90402e7234753ae70eea99ff17e9fd9b
Maciek-Sawicki <macieksawickim@gmail.com>
2024-10-21 20:16:14 +0200

    add session

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from 'express'; 
+import session from 'express-session';
 import gameRoutes from './routes/gameRoutes.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(session({
+  secret: 'your-secret-key', // Zmień na bezpieczny sekret w produkcji
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      secure: false, // Ustaw na true, jeśli używasz HTTPS
+      httpOnly: true,
+      maxAge: 3600000, // Czas życia sesji w milisekundach
+  },
+}));
 
 app.use('/api', gameRoutes); 
 
